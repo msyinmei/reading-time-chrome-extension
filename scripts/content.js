45f554f8d42dacfcc8c75fcf300336311150494c
Yin Mei <yin.codes@gmail.com>
2024-08-05 21:18:17 -0400

    extended script to allow echosystems.substack.com

---
+++ scripts/content.js
@@ -0,0 +1,44 @@
+const article = document.querySelector("article");
+const blogheader = document.querySelector(".post-header");
+const blog = document.querySelector(".available-content");
+
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (article) {
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  // matchAll returns an iterator, convert to array to get word count
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  // Use the same styling as the publish information in an article's header
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
+
+if (!article && body) {
+  const text = body.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  // matchAll returns an iterator, convert to array to get word count
+  const wordCount = [...words].length;
+  console.log('wordCount', wordCount)
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  // Use the same styling as the publish information in an article's header
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  // Support for recipe docs
+  const heading = blogheader.querySelector("h1");
+
+  (heading).insertAdjacentElement("afterend", badge);
+}
